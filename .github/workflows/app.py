3a03895d635f9f573c3af8408d118a3a0648f078
WSTEVENWOLFE <165227083+WSTEVENWOLFE@users.noreply.github.com>
2024-03-28 16:46:26 -0500

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,7 +1,7 @@
 from webex_bot.webex_bot import WebexBot
 from gpt import gpt
 
-bot = WebexBot("Your webex bot token")
+bot = WebexBot("NGVmZDc2ZDctOGU3Ny00Njk1LWFlZTQtNmVhYWIxZjBhZjc1MGMxZGY2ZWQtNmM4_P0A1_d5fe8096-9b9a-43dd-bc72-9f86104a819b")
 bot.commands.clear()
 bot.add_command(gpt())
 bot.help_command = gpt()
